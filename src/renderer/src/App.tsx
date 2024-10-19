0e56ccc1fb8f4ba49e008c42ae3a1c581ca16e07
Ashutosh Dubey <ashutosh@richpanel.com>
2024-10-19 21:34:01 +0530

    added shad cn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from './../../components/ui/button'
+
 function App(): JSX.Element {
-  return <></>
+  return (
+    <div className="flex w-screen h-screen bg-transparent justify-center items-center">
+      <div className="flex justify-center items-center">
+        <Button>Test</Button>
+      </div>
+    </div>
+  )
 }
 
 export default App
