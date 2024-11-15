ccaa8b6a24f41871116b2fe71dc12ea0956c2e73
Ankit Raj <imankitrj@gmail.com>
2024-11-15 19:18:12 +0530

    uniform ui

--- src/renderer/src/pages/Settings/SettingsSidebar/index.tsx
+++ src/renderer/src/pages/Settings/SettingsSidebar/index.tsx
@@ -1,13 +1,37 @@
+import { Button } from '@components/ui/button'
 import { SettingSection } from './SettingSection'
 import { settingsSectionsList } from '@renderer/lib/constants'
+import { FiSearch } from 'react-icons/fi'
 import { RiSettings2Fill } from 'react-icons/ri'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@components/ui/tooltip'
 
 export const SettingsSidebar = () => {
   return (
-    <div className="bg-opacity-100 text-white w-64 h-full flex flex-col gap-10">
-      <div className="mt-12 p-2 flex gap-2 items-center">
-        <RiSettings2Fill className="text-3xl opacity-70" />
-        <span className="text-3xl opacity-80">Settings</span>
+    <div className="bg-opacity-100 text-white w-64 h-full flex flex-col">
+      <div className="mb-2 mx-1 py-2 px-2 border-b border-white/5">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <RiSettings2Fill className="text-xl text-white/40" />
+            <h1 className="text-lg font-semibold text-white/40">Settings</h1>
+          </div>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="h-8 w-8 p-0 text-white/40 hover:text-white/90 hover:bg-white/10 rounded-full [&_svg]:size-6"
+                  onClick={() => {}}
+                >
+                  <FiSearch className="text-2xl text-white/40" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="left" className="bg-[#1a1a1a] text-[11px] rounded-lg p-2">
+                <p className="text-white">Search</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
       <div className="bg-opacity-60 h-screen flex flex-col overflow-y-scroll">
         {settingsSectionsList.map((section, index) => (
